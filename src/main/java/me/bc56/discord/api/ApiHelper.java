096541d65cea0eeb65061ba29119a278b0f9768c
Bryce Clark <bryce@bc56.me>
2020-09-20 02:08:34 -0500

    makeRequest helper method

--- src/main/java/me/bc56/discord/api/ApiHelper.java
+++ src/main/java/me/bc56/discord/api/ApiHelper.java
@@ -1,19 +1,23 @@
 package me.bc56.discord.api;
 
+import checkers.nullness.quals.NonNull;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import me.bc56.discord.model.exception.DiscordApiException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Objects;
 
 public class ApiHelper {
     private static Logger log = LoggerFactory.getLogger(ApiHelper.class);
 
-    public static DiscordApiException getException(Response<?> requestResponse) {
+    //Creates and returns a DiscordAPIException for a failed request to the Discord API
+    public static DiscordApiException getException(@NonNull Response<?> requestResponse) {
         Objects.requireNonNull(requestResponse.errorBody(),
                 "Request has a null error body. Are you sure this is an unsuccessful request?");
 
@@ -22,12 +26,29 @@ public class ApiHelper {
             jsonError = (new Gson()).fromJson(requestResponse.errorBody().string(), JsonObject.class);
         } catch (IOException e) {
             log.error("Problem parsing JSON body of HTTP response", e);
-            throw new RuntimeException("How did it fail to turn it into a string?!");
+            throw new UncheckedIOException(e);
         }
 
         int jsonCode = jsonError.get("code").getAsInt();
         String jsonMessage = jsonError.get("message").getAsString();
 
         return new DiscordApiException(requestResponse.code(), jsonCode, jsonMessage);
+    }
+
+    public static <E> E makeRequest(@NonNull Call<E> request) throws DiscordApiException {
+        try {
+            Response<E> response = request.execute();
+
+            if (response.isSuccessful()) {
+                return response.body();
+            }
+            else {
+                // Bad response if reached
+                throw getException(response);
+            }
+        } catch (IOException e) {
+            log.error("Problem while sending HTTP request to {}", request.request().url(), e);
+            throw new UncheckedIOException(e);
+        }
     }
 }
--- src/main/java/me/bc56/discord/api/Guilds.java
+++ src/main/java/me/bc56/discord/api/Guilds.java
@@ -1,7 +1,5 @@
 package me.bc56.discord.api;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import me.bc56.discord.model.Guild;
 import me.bc56.discord.model.Snowflake;
 import me.bc56.discord.model.exception.DiscordApiException;
@@ -9,9 +7,6 @@ import me.bc56.discord.service.DiscordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import retrofit2.Call;
-import retrofit2.Response;
-
-import java.io.IOException;
 
 public class Guilds extends DiscordApiAccessor {
     private static final Logger log = LoggerFactory.getLogger(Guilds.class);
@@ -25,36 +20,23 @@ public class Guilds extends DiscordApiAccessor {
     }
 
     public Guild get(Snowflake guildId) throws DiscordApiException {
-        Call<Guild> request = discord.getGuild(
-                String.valueOf(guildId.id),
-                false
-        );
-
-        try {
-            Response<Guild> response = request.execute();
-
-            if (response.isSuccessful()) {
-                return response.body();
-            }
-
-            // Bad response if reached
-            throw ApiHelper.getException(response);
-        } catch (IOException e) {
-            log.error("Exception while sending HTTP request to discord/getGuild", e);
-            //TODO: Proper handling of this situation
-            return null;
-        }
+        return this.get(guildId, false);
     }
 
-    public void getWithCounts(Snowflake guildId) throws DiscordApiException {
+    public Guild get(Snowflake guildId, boolean withCounts) throws DiscordApiException {
+        Call<Guild> request = discord.getGuild(
+                String.valueOf(guildId.id),
+                withCounts
+        );
 
+        return ApiHelper.makeRequest(request);
     }
 
     public void create() throws DiscordApiException {
 
     }
 
-    public void delete() {
+    public void delete() throws DiscordApiException {
 
     }
 }
